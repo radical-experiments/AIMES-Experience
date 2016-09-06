03ffa0014bf5599376b14805b0879a5834167486
Andre Merzky <andre@merzky.net>
2016-09-06 23:26:56 +0200

    iterate install script(s)

---
+++ synapse_integration_testing/bin/automate_rcstack_installation_AM.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+deactivate || true
+
+cwd=`pwd`
+base=$HOME/radical/
+exp=experiments/AIMES-Experience
+
+cd $base/$exp
+rm -rf ve/
+virtualenv ve/
+. ve/bin/activate
+
+install()
+{
+    dir=$1
+    branch=$2
+
+    echo
+    printf "--------------------------------------------\n"
+    printf "INSTALL  %-20s @ %s\n" $dir $branch
+    printf "--------------------------------------------\n"
+
+    builtin cd $base/$dir
+    git co $branch
+    git pull
+    pip uninstall -y . || true
+    pip install .
+}
+
+
+install  radical.utils        experiment/aimes
+install  radical.saga         experiment/aimes
+install  radical.pilot.aimes  experiment/aimes
+install  aimes.skeleton       feature/task_prof
+install  aimes.bundle         master
+install  aimes.emgr           feature/osg_run
+
+cd $cwd
+
--- synapse_integration_testing/bin/automate_rcstack_installation_MT.sh
+++ synapse_integration_testing/bin/automate_rcstack_installation_MT.sh
@@ -9,45 +9,46 @@ rm -r ~/Virtualenvs/AIMES-EXPERIENCE
 virtualenv ~/Virtualenvs/AIMES-EXPERIENCE
 . ~/Virtualenvs/AIMES-EXPERIENCE/bin/activate
 
-# Install RP, branch experiment/aimes
-cd radical.pilot/
+# Install RU, branch experiment/aimes
+cd radical.utils/
 git checkout experiment/aimes
 git pull
-pip install -U .
+pip install .
 cd ..
 
 # Install SAGA, branch experiment/aimes
 cd saga-python/
 git checkout experiment/aimes
 git pull
-pip install -U --no-deps .
+pip install .
 cd ..
 
-# Install RU, branch experiment/aimes
-cd radical.utils/
+# Install RP, branch experiment/aimes
+cd radical.pilot/
 git checkout experiment/aimes
 git pull
-pip install -U --no-deps .
+pip install .
 cd ..
 
 # Installing Skeleton, branch feature/task_prof
 cd radical.skeleton/
 git checkout feature/task_prof
 git pull
-pip install -U --no-deps .
+pip install .
 cd ..
 
 # install aimes.bundle:
 cd aimes.bundle/
-pip install -U .
+pip install .
 cd ..
 
 # Installing aimes.emgr, branch feature/osg_run
 cd aimes.emgr/
 git checkout feature/osg_run
 git pull
-pip install -U --no-deps .
+pip install .
 cd ..
 
 # Go back to the initial directory
 cd ~/Projects/RADICAL/github/experiments/AIMES-Experience/synapse_integration_testing/bin
+
