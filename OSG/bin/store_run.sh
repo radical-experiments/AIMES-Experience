e39095f73a9f1c3d1a2e3e64c5e79fd2f9ab917c
Matteo Turilli <matteo.turilli@gmail.com>
2016-11-29 21:52:55 +0100

    Clean up OSG experiment executables

---
+++ OSG/bin/store_run.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+ROOT='/home/mturilli/experiments/AIMES-Experience/OSG/bin'
+TMP='/tmp/profiles'
+STORE='/home/mturilli/experiments/AIMES-Experience/OSG/raw_data'
+EXP='experiment_10'
+
+sid=$1
+run=$2
+bot=$3
+
+cd $TMP
+echo $PWD
+
+echo "radicalpilot-fetch-profiles -c $ROOT $sid"
+radicalpilot-fetch-profiles -c $ROOT $sid
+
+echo "cp $ROOT/$run/$sid.json $sid/"
+cp $ROOT/$run/$sid.json $sid/
+
+echo "mv $sid $STORE/$EXP/$bot/"
+mv $sid $STORE/$EXP/$bot/
+
+ls -altr $STORE/$EXP/$bot/
+ls $STORE/$EXP/$bot/$sid/
+cd $ROOT
