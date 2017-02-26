7a14903c99abc3d60a5d4ae1ff9c85d79ae33021
Matteo Turilli <matteo.turilli@gmail.com>
2017-02-26 18:48:52 +0100

    Experiment 18: 128 pilots; 128,256,512,1024,2048 units

--- old/synapse_integration_testing/bin/automate_rcstack_installation_MT_vm.sh
+++ old/synapse_integration_testing/bin/automate_rcstack_installation_MT_vm.sh
@@ -1,6 +1,6 @@
 # OSX only with brew
-export LDFLAGS="-L/usr/local/opt/openssl/lib"
-export CPPFLAGS="-I/usr/local/opt/openssl/include"
+#export LDFLAGS="-L/usr/local/opt/openssl/lib"
+#export CPPFLAGS="-I/usr/local/opt/openssl/include"
 
 cd ~/github
 
@@ -8,6 +8,10 @@ deactivate
 rm -r ~/ve/AIMES-EXPERIENCE
 virtualenv ~/ve/AIMES-EXPERIENCE
 . ~/ve/AIMES-EXPERIENCE/bin/activate
+
+# Avoid error about setuptools
+pip install --upgrade pip
+pip install --upgrade setuptools
 
 # Install RU, branch experiment/aimes
 cd radical.utils/
@@ -50,7 +54,7 @@ pip install .
 cd ..
 
 # Go back to the initial directory
-cd ~/experiments/AIMES-Experience/synapse_integration_testing/bin
+cd /home/mturilli/experiments/AIMES-Experience/old/synapse_integration_testing/bin
 
 echo "RP: `radicalpilot-version`"
 echo "RS: `sagapython-version`"
