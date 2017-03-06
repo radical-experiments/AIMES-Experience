9a602f217dcf719a6f7af6618b7af5fd55a54750
Matteo Turilli <matteo.turilli@gmail.com>
2017-03-06 16:28:10 +0100

    Done. Experiment 18: 128 pilots; 128,256,512,1024,2048 units

--- OSG/bin/setup.sh
+++ OSG/bin/setup.sh
@@ -3,4 +3,4 @@ export RADICAL_VERBOSE="DEBUG"
 export RADICAL_PILOT_PROFILE="True"
 export RADICAL_PILOT_DBURL="mongodb://matteo:matteo@ds035385.mlab.com:35385/aimes-aws"
 export RADICAL_PROFILE="True"
-
+export RADICAL_SAGA_PTY_VERBOSE="DEBUG"
